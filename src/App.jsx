4a422ce267354ad2c4ffe8f004a76f6422d16653
Lalaf03 <laura.fernandez_gom@uao.edu.co>
2024-10-03 20:04:04 -0500

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,3 @@
-/*import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}*/
 import React from "react";
 import "./Styles.css";
 import {
@@ -39,8 +6,6 @@ import {
   Routes,
   NavLink,
 } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faInstagram } from "@fortawesome/free-brands-svg-icons";
 import SobreMi from "./assets/componentes/SobreMi";
 import Habilidades from "./assets/componentes/Habilidades";
 import Projects from "./assets/componentes/Projects";
@@ -61,7 +26,11 @@ function App() {
           </Routes>
         </main>
 
-        <footer></footer>
+        <footer>
+          <p>Desarrollo de Experiencias Multimedia para la Web</p>
+          <p>Parcial 2 Laura Fernández Gómez</p>
+          <p>laura.fernandez_gom@uao.edu.co</p>
+        </footer>
       </div>
     </Router>
   );
